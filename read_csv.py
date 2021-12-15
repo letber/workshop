0114c58460779e4e5b0cdaaee4faea9ac5bc236a
Siromanec <91615647+Siromanec@users.noreply.github.com>
2021-12-15 12:30:13 +0200

    added return of date, task, times, and success

--- read_csv.py
+++ read_csv.py
@@ -1,25 +1,23 @@
-import sys
+"""current date reader"""
 import datetime
+from typing import List
 
-def read_csv(file):
+def read_csv(file:str)->List(tuple):
     """
     reads csv
-
-
-    
     """
     date = datetime.date.today().strftime("%d/%m/%Y").replace("/",".")
+    current_day = []
     with open(file, 'r') as file_contents:
         file_contents.readline()
         data = file_contents.read().splitlines()
-
         for line in data:
             line_list = line.split(",")
             if line_list[0] == date:
                 task = line_list[1]
                 times = line_list[2]
- 
-                
-            pass
-x = 'a,b,c'
-print(x.split(","))   
+                if line_list[3] != "*":
+                    success = False
+                elif line_list[3] == "*": success = True
+                current_day.append((date, task, times, success))
+    return current_day
