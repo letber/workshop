2922a560195f08e05689592c62967067279248a1
MikeTr04 <regularinternetuser@gmail.com>
2021-12-15 12:09:26 +0200

    Support of command to create new tasks

--- main.py
+++ main.py
@@ -6,7 +6,17 @@ import os
 
 
 if __name__ == "__main__":
-    with open('goals.csv', 'w', newline='') as file1:
-        writer = csv.writer(file1)
-        writer.writerow(["Days", "Number of times", "Success", "Failure"])
+    file1 = open('goals.csv', 'w', newline='')
+    writer = csv.writer(file1)
+    writer.writerow(["Days", "Number of times", "Success", "Failure"])
+        
+    inp = input()
+    if inp.startswith('create'):
+        strings = inp.split(' ')
+        num = int(strings[-1])
+        task = " ".join(strings[1:-1])
+        writer.writerow([f'{task} {num} times', 
+                        0, 
+                        "*" if 0 >= num else "", 
+                        "*" if 0 < num else ""])
     
